ba469ac763e45140edff613d0e16b45d3e54c011
MariKush <43176370+MariKush@users.noreply.github.com>
2022-05-17 00:06:40 +0300

    beautify achievementService. move validation for NotEmpty achievementConditions to controller

--- Lab1/src/main/java/com/trustmenet/repositories/entities/Achievement.java
+++ Lab1/src/main/java/com/trustmenet/repositories/entities/Achievement.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Data
@@ -16,5 +17,6 @@ public class Achievement {
     private int id;
     private String name;
     private String description;
+    @NotEmpty
     private List<AchievementCondition> achievementConditions;
 }
